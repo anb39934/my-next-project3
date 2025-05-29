48104e64947df0cd807608f0ffcddc9eee745bcf
anb39934 <anb39934@gmail.com>
2025-05-29 17:32:29 +0900

    サイト全体にrevalidateを設定する

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import styles from "./page.module.css";
-import Image from "next/image";
-import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS_LIMIT } from "@/app/_constants";
-import NewsList from "@/app/_components/NewsList";
-import ButtonLink from "@/app/_components/ButtonLink";
+import styles from './page.module.css';
+import Image from 'next/image';
+import { getNewsList } from '@/app/_libs/microcms';
+import { TOP_NEWS_LIMIT } from '@/app/_constants';
+import NewsList from '@/app/_components/NewsList';
+import ButtonLink from '@/app/_components/ButtonLink';
+
+export const revalidate = 60;
 
 export default async function Home() {
   const data = await getNewsList({
@@ -26,7 +28,6 @@ export default async function Home() {
           height={1200}
         />
       </section>
-
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
         <NewsList news={data.contents} />
